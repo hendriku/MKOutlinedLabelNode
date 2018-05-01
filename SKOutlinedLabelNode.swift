727a7fa7cced34c9bbe89f325e76ec91c7642b29
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-05-01 16:36:52 +0200

    Remove debugs

--- SKOutlinedLabelNode.swift
+++ SKOutlinedLabelNode.swift
@@ -61,9 +61,6 @@ class SKOutlinedLabelNode: SKLabelNode {
                     border.zPosition = -10
                 }
                 
-                print(self.zPosition)
-                print(border.zPosition)
-                
                 addChild(border)
                 
                 self.border = border
